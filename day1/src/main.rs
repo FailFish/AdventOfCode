83a29e2f982d4c9bad701da9e1d8ce42f3d886d3
FailFish <likeinstein42@gmail.com>
2022-12-03 01:34:54 +0900

    day1 solution

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs::File, io::{BufReader, BufRead, Error}};
+
+fn main() -> Result<(), Error> {
+    let file = File::open("input")?;
+    let mut buf_reader = BufReader::new(file);
+    let mut buf = String::new();
+    let mut sum = 0u64;
+    let mut maxsum = 0u64;
+    loop {
+        match buf_reader.read_line(&mut buf)? {
+            0 => break,
+            1 => {
+                maxsum = if maxsum > sum { maxsum } else { sum };
+                sum = 0;
+            },
+            _ => sum += buf.trim_end().parse::<u64>().expect(&format!("buf: {}", buf)),
+        }
+        buf.clear();
+    }
+    println!("{}", maxsum);
+    Ok(())
 }
